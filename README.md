1651073bbb32046a2d252e285664f917aa2d28a5
Way2nnadi <ucheunnadi@gmail.com>
2017-01-06 19:43:37 -0800

    [add] solution

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# kofile-assessment
+# Kofile-assessment
+
+## Getting Started
+```
+npm install
+npm run build
+npm start
+```
+
+### Running Tests
+```
+npm test
+```
+
+### Solutions
+> Instructions for the solutions can be found in solutions.js
+
+### Necessary Software
+> Postman
+
+
+
---
+++ test/index.js
@@ -0,0 +1,19 @@
+import assert from 'assert';
+import request from 'request';
+import chai from 'chai';
+
+import '../lib/server.js';
+
+let testUri = `http://127.0.0.1:${process.env.PORT || 3000}`
+
+describe('Testing Node Server', () => {
+  it('should return 200', done => {
+    request({
+        url: testUri,
+        method: "GET",
+    }, (err, res, body) => {
+        assert.equal(200, res.statusCode);
+        done();
+    });
+  });
+});
